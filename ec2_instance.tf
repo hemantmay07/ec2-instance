2ee954d35bf2785cb9f00e76e4e0ced6594c1312
hemantmay07 <53380787+hemantmay07@users.noreply.github.com>
2022-11-07 19:50:21 +0000

    Add files via upload

---
+++ ec2_instance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+   region = "us-east-1"
+   access_key = "AKIAQJER6OZHT2WL7DMW"
+   secret_key = "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"
+}
+
+resouce ec2_instance{
+    ami = "ami-09d3b3274b6c5d4aa"
+    instance_type = "ami-09d3b3274b6c5d4aa"
+}
